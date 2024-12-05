e7baf5d8ea456ec85f6ae8b1f58ba3ca99415c44
Ariashka <ariana0tsarova@gmail.com>
2024-12-05 14:30:51 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,11 @@ import json
 with open("stars.json", "r", encoding="UTF-8") as file:
     data = json.load(file)
 
-count = 0
-open=True
+def main():
+  count = 0
+  open = True
 
-def menu():
+  def menu():
     print("""
     Меню:
     1 - Вывести все записи
@@ -16,9 +17,9 @@ def menu():
     5 - Выйти из программы
     """)
 
-def all():#Вывести все записи
-    global count
-    for star in data:
+  def all():#Вывести все записи
+      global count
+      for star in data:
         print(f"""
             Номер записи: {star['id']},
             Общее название звезды: {star['name']},
@@ -26,41 +27,41 @@ def all():#Вывести все записи
             Можно ли увидеть звезду без телескопа: {star['is_visible']},
             Солнечный радиус звезды: {star['radius']},
             """)
-    count += 1
+      count += 1
 
-def pole():#Вывести запись по полю
-    global count
-    search = input("Введите id: ")
+  def polo():
+      global count
+      search = input("Введите id: ")
 
-    for i, star in enumerate(data):
+      for i, star in enumerate(data):
         if star['id'] == search:
-            print(f"""
+          print(f"""
             Номер записи: {star['id']},
             Общее название звезды: {star['name']},
             Название созвездия: {star['constellation']},
             Можно ли увидеть звезду без телескопа: {star['is_visible']},
             Солнечный радиус звезды: {star['radius']},
             """)
-            print(f"Позиция в словаре: {i}")
-            break
+          print(f"Позиция в словаре: {i}")
+          break
         else:
-            print("Не найдено")
-    count += 1
+           print("Не найдено")
+      count += 1
 
-def add():#Добавить запись
-    global count
-    new_id = int(input("Введите id: "))
-    for star in data:
+  def add():
+      global count 
+      new_id = input("Введите новый id: ")
+      for star in data:
         if star['id'] == new_id:
-            print("Звезда уже добавлена")
-            break
+          print("Звезда уже добавлена")
+          break
         else:
-            new_name = str(input("Название звезды: "))
-            new_constellation = str(input("Название созвездия: "))
-            new_is_visible = bool(input("Можно ли увидеть звезду без телескопа: "))
-            new_radius = float(input("Солнечный радиус звезды: "))
+          new_name = str(input("Название звезды: "))
+          new_constellation = str(input("Название созвездия: "))
+          new_is_visible = bool(input("Можно ли увидеть звезду без телескопа: "))
+          new_radius = float(input("Солнечный радиус звезды: "))
 
-            new_star = {
+          new_star = {
                 "id": new_id,
                 "name": new_name,
                 "constellation": new_constellation,
@@ -68,48 +69,47 @@ def add():#Добавить запись
                 "radius": new_radius
             }
 
-            data.append(new_star)
-            with open("stars.json", "w",encoding="UTF-8") as new_file:
-              json.dump(data, new_file, ensure_ascii = False, indent=4)
-              print("Запись добавлена")
-    count += 1
-    
-def delete(): #Удалить запись
-    global count
-    del_id = int(input("Введите id для удаления: "))
-    for star in data:
+          data.append(new_star)
+          with open("stars.json", "w",encoding="UTF-8") as new_file:
+            json.dump(data, new_file, ensure_ascii = False, indent=4)
+            print("Запись добавлена")
+      count += 1
+
+  def delete():
+      global count
+      del_id = input("Введите id для удаления: ")
+      for star in data:
         if star['id'] == del_id:
-            data.remove(star)
-            with open("stars.json", "w",encoding="UTF-8") as new_file:
-                json.dump(data, new_file, ensure_ascii = False, indent=4)
-            print("Запись удалена")
-            break
+          data.remove(star)
+          with open("stars.json", "w",encoding="UTF-8") as new_file:
+              json.dump(data, new_file, ensure_ascii = False, indent=4)
+          print("Запись удалена")
+          break 
         else:
-                print("Запись не найдена")
-    count += 1
+          print("Запись не найдена")
+      count += 1
     
-def end():  # Выйти из программы
-    print("Выход из программы")
-    print("Количество выполненных операций с записями: ", count)
-    open = False
-    
-def main():
-    while open:
-        menu()
+  def end():
+      print("Выход из программы")
+      print("Количество выполненных операций с записями: ", count)
 
-        n = int(input("Введите пункт: "))
+  while open:
+      menu()
 
-        if n == 1:
-          all()
-        elif n == 2:
-          pole()
-        elif n == 3:
-          add()
-        elif n == 4:
-          delete()
-        elif n == 5:
-          end()
-        else:
-          print("Ошибка")
+      n = input("Введите номер пункта: ")
 
-main()
+      if n == 1:
+        all()
+      elif n == 2:
+        pole()
+      elif n == 3:
+        add()
+      elif n == 4:
+        delete()
+      elif n == 5:
+        end()
+      else:
+        print("Ошибка")
+
+if __name__ == "__main__":
+  main()
